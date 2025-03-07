b90b8cffc53c90adfaae130fa27f381688bc030d
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 06:56:47 +0330

    Update admin_handler.py

--- handlers/admin_handler.py
+++ handlers/admin_handler.py
@@ -4,16 +4,15 @@ from database.songs_db import load_songs
 DB_PATH = "songs.json"
 
 async def handle_admin_commands(event):
+    print(f"Received command: {event.text}")  # اضافه کردن خط برای بررسی
     if event.text == "/list":
         if os.path.exists(DB_PATH):
             await event.reply("Here is the songs database:", file=DB_PATH)
         else:
             await event.reply("Database file not found.")
-
     elif event.file and event.file.name == "songs.json":
         await event.download_media(file=DB_PATH)
         songs = load_songs()
         await event.reply(f"Database updated with {len(songs)} songs.")
-
     else:
         await event.reply("Unknown command.")
