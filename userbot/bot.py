78083569232d7f2be9d201b0248530e29f006032
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 15:29:39 +0330

    Update bot.py

--- userbot/bot.py
+++ userbot/bot.py
@@ -6,7 +6,7 @@ from database.songs_db import add_song, load_songs
 from handlers.message_handler import handle_new_song
 from handlers.admin_handler import handle_admin_commands
 from handlers.delete_handler import handle_deleted_message
-from handlers.forward_handler import forward_to_inlinebot  # ایمپورت هندلر فوروارد
+from handlers.forward_handler import handle_forward  # اضافه شد
 from scheduler.nightly_job import start_nightly_job
 
 load_dotenv()
@@ -17,29 +17,29 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 client = TelegramClient('bot', API_ID, API_HASH)
 
+
 @client.on(events.NewMessage)
 async def new_message_handler(event):
     await handle_new_song(event, client)
+    await handle_forward(event, client)  # اضافه شد
+
 
 @client.on(events.NewMessage(from_users=ADMIN_ID))
 async def admin_commands_handler(event):
     await handle_admin_commands(event, client)
 
+
 @client.on(events.MessageDeleted())
 async def deleted_message_handler(event):
     await handle_deleted_message(event)
 
-# فراخوانی تابع فوروارد فقط زمانی که پیامی دریافت شد
-@client.on(events.NewMessage)
-async def forward_handler(event):
-    # فقط از کانال مشخص شده و پیام‌های صوتی فوروارد میشه
-    await forward_to_inlinebot(client)
 
 async def main():
     await client.start()
     print("Userbot is running...")
     start_nightly_job(client)
     await client.run_until_disconnected()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
