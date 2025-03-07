2226e0bedbb29d5465e6ca50a10e2e5325098173
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 17:05:26 +0330

    Update bot.py

--- inlinebot/bot.py
+++ inlinebot/bot.py
@@ -16,30 +16,22 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 GROUP_ID = int(os.getenv("GROUP_ID"))
 
-bot = Bot(
-    token=TOKEN,
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-)
+# راه‌اندازی ربات
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
 
+# تنظیمات Router
 router = Router()
 dp.include_router(router)
 
-@router.message()
-async def test_log(message: Message):
-    print(f"[INLINEBOT] Received message from chat ID: {message.chat.id}")
-
-# هندلر پیام‌های گروه مشترک
 @router.message(F.chat.id == GROUP_ID)
 async def group_message_handler(message: Message):
     await handle_new_song(message)
 
-# هندلر دستورات ادمین (مثل /list)
 @router.message(F.text.startswith("/"))
 async def admin_command_handler(message: Message):
     await handle_admin_commands(message)
 
-# هندلر اینلاین مود
 @dp.inline_query()
 async def inline_query_handler(inline_query):
     await handle_inline_query(inline_query, bot)
