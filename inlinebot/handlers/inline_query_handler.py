2e417ab169075fb806a3a0145befe90c32ce412b
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 20:17:56 +0330

    Update inline_query_handler.py

--- inlinebot/handlers/inline_query_handler.py
+++ inlinebot/handlers/inline_query_handler.py
@@ -1,3 +1,4 @@
+import os
 import json
 from aiogram import types
 
@@ -8,26 +9,19 @@ def load_songs():
         return json.load(db_file)
 
 async def handle_inline_query(inline_query: types.InlineQuery, bot):
-    query = inline_query.query.strip().lower()  # حذف فاصله‌های اضافی
-
-    if len(query) < 3:
-        await bot.answer_inline_query(
-            inline_query.id,
-            results=[],
-            cache_time=1,
-            switch_pm_text="حداقل 3 حرف وارد کنید.",
-            switch_pm_parameter="start"
-        )
-        return
+    query = inline_query.query.strip().lower()
 
     songs = load_songs()
-    matched_songs = [
-        song for song in songs
-        if query in song["title"].lower() or query in song["singer"].lower()
-    ]
+    if query:
+        matched_songs = [
+            song for song in songs
+            if query in song["title"].lower() or query in song["singer"].lower()
+        ]
+    else:
+        matched_songs = songs
 
     results = []
-    for index, song in enumerate(matched_songs[:20]):  # محدود به 10 نتیجه
+    for index, song in enumerate(matched_songs[:10]):  # فقط 10 نتیجه
         try:
             results.append(
                 types.InlineQueryResultAudio(
@@ -45,4 +39,4 @@ async def handle_inline_query(inline_query: types.InlineQuery, bot):
         inline_query.id,
         results=results,
         cache_time=1
-                )
+    )
