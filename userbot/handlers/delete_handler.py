1f2cac70cb5962e8013d806f473c83685b07d9f3
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 19:08:48 +0330

    Update delete_handler.py

--- userbot/handlers/delete_handler.py
+++ userbot/handlers/delete_handler.py
@@ -2,9 +2,7 @@ from database.songs_db import remove_song
 
 async def handle_deleted_message(event):
     if event.deleted_ids:
-        chat = await event.get_chat()
-        channel_username = chat.username or "Private Channel"
-
+        chat_id = event.chat_id
         for msg_id in event.deleted_ids:
-            remove_song(msg_id, channel_username)
-            print(f"Removed song with message_id {msg_id} from channel {channel_username}")
+            remove_song(msg_id, chat_id)
+            print(f"[USERBOT] Removed song with message_id {msg_id} from chat_id {chat_id}")
