c438775ec61298f88647b49bd76b5a6ca723ea39
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 13:04:06 +0330

    Update songs_db.py

--- database/songs_db.py
+++ database/songs_db.py
@@ -1,36 +1,31 @@
-import json
 import os
+import json
 
 DB_PATH = "songs.json"
 
+# ساخت دیتابیس در صورت عدم وجود
 def create_db_if_not_exists():
     if not os.path.exists(DB_PATH):
-        with open(DB_PATH, 'w', encoding='utf-8') as db_file:
+        with open(DB_PATH, "w", encoding="utf-8") as db_file:
             json.dump([], db_file, ensure_ascii=False, indent=4)
 
-create_db_if_not_exists()
-
+# بارگذاری دیتابیس
 def load_songs():
     if not os.path.exists(DB_PATH):
-        with open(DB_PATH, "w", encoding="utf-8") as db_file:
-            json.dump([], db_file)
+        create_db_if_not_exists()
     with open(DB_PATH, "r", encoding="utf-8") as db_file:
         return json.load(db_file)
 
-def save_songs(songs):
-    with open(DB_PATH, "w", encoding="utf-8") as db_file:
-        json.dump(songs, db_file, indent=4, ensure_ascii=False)
-
+# افزودن آهنگ به دیتابیس
 def add_song(title, singer, file_id, duration, channel, message_id):
     songs = load_songs()
-    song_data = {
+    songs.append({
         "title": title,
         "singer": singer,
         "file_id": file_id,
         "duration": duration,
         "channel": channel,
         "message_id": message_id
-    }
-    songs.append(song_data)
-    save_songs(songs)
-
+    })
+    with open(DB_PATH, "w", encoding="utf-8") as db_file:
+        json.dump(songs, db_file, ensure_ascii=False, indent=4)
