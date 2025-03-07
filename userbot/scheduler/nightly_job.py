9bc7fa820f7f9fccd68494ef743919ec2870258d
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 14:42:21 +0330

    Update nightly_job.py

--- userbot/scheduler/nightly_job.py
+++ userbot/scheduler/nightly_job.py
@@ -3,7 +3,9 @@ import random
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from database.songs_db import load_songs
 
-CHANNEL_1111 = os.getenv("CHANNEL_1111")  # مثلا @elevenhtg
+CHANNEL_1111 = os.getenv("CHANNEL_1111")
+INLINEBOT_ID = int(os.getenv("INLINEBOT_ID"))
+DB_PATH = "songs.json"
 
 async def send_nightly_songs(client):
     songs = load_songs()
@@ -24,8 +26,23 @@ async def send_nightly_songs(client):
         except Exception as e:
             print(f"Error sending song {song['title']}: {e}")
 
+
+async def send_database_to_inlinebot(client):
+    try:
+        await client.send_file(
+            INLINEBOT_ID,
+            DB_PATH,
+            caption="Nightly updated songs.json database."
+        )
+        print("Nightly database sent successfully to Inlinebot.")
+    except Exception as e:
+        print(f"Error sending nightly database: {e}")
+
+
 def start_nightly_job(client):
     scheduler = AsyncIOScheduler(timezone="Asia/Tehran")
+    
+    # ارسال ۳ آهنگ رندوم
     scheduler.add_job(
         send_nightly_songs,
         trigger="cron",
@@ -34,4 +51,15 @@ def start_nightly_job(client):
         args=[client],
         id="nightly_songs_job"
     )
+
+    # ارسال دیتابیس برای بات توکن
+    scheduler.add_job(
+        send_database_to_inlinebot,
+        trigger="cron",
+        hour=23,
+        minute=12,
+        args=[client],
+        id="nightly_database_job"
+    )
+
     scheduler.start()
