bcce088e19e9a2561cf4ab333beb3e3714a1fed0
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 18:49:53 +0330

    Update inline_query_handler.py

--- inlinebot/handlers/inline_query_handler.py
+++ inlinebot/handlers/inline_query_handler.py
@@ -1,25 +1,30 @@
 import json
 from aiogram import types
-from database.songs_db import load_songs
+from aiogram.types import InlineQueryResultAudio
+
+DB_PATH = "songs.json"
 
 async def handle_inline_query(inline_query: types.InlineQuery, bot):
     query = inline_query.query.lower()
-    results = []
-    songs = load_songs()
+    print(f"[INLINE] Inline query received: {query}")
 
-    for idx, song in enumerate(songs):
+    if not query:
+        await inline_query.answer([], cache_time=1)
+        return
+
+    songs = json.load(open(DB_PATH, "r", encoding="utf-8"))
+    results = []
+
+    for song in songs:
         if query in song["title"].lower() or query in song["singer"].lower():
             results.append(
-                types.InlineQueryResultCachedAudio(
-                    id=str(idx),
+                InlineQueryResultAudio(
+                    id=str(song["message_id"]),
                     audio_file_id=song["file_id"],
                     title=song["title"],
-                    performer=song["singer"]
+                    performer=song["singer"],
+                    duration=song["duration"]
                 )
             )
 
-    await bot.answer_inline_query(
-        inline_query.id,
-        results=results[:50],  # حداکثر ۵۰ نتیجه
-        cache_time=1
-    )
+    await inline_query.answer(results, cache_time=1)
