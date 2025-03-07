0f45ba09d9e7ebaac6e5f3d17b1114a91da7da79
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-07 18:51:13 +0330

    Update admin_handler.py

--- inlinebot/handlers/admin_handler.py
+++ inlinebot/handlers/admin_handler.py
@@ -1,35 +1,19 @@
-import os
-import json
+from handlers.database_handler import send_database, update_database
 from aiogram import types
-from aiogram.types import FSInputFile
-from database.songs_db import create_db_if_not_exists
+import os
 
-DB_PATH = "songs.json"
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
-async def handle_admin_commands(message: types.Message):
+async def handle_admin_commands(message: types.Message, bot):
     if message.from_user.id != ADMIN_ID:
         await message.reply("شما دسترسی ندارید.")
         return
 
-    if message.text.strip() == "/list":
-        print(f"[ADMIN_HANDLER] /list command received from {message.from_user.id}")
+    if message.text == "/list":
+        await send_database(message, bot)
 
-        create_db_if_not_exists()
+    elif message.document:
+        await update_database(message, bot)
 
-        if not os.path.exists(DB_PATH):
-            await message.reply("دیتابیس پیدا نشد.")
-            return
-
-        songs = json.load(open(DB_PATH, "r", encoding="utf-8"))
-        if not songs:
-            await message.reply("دیتابیس خالی است.")
-            return
-
-        await message.answer_document(
-            FSInputFile(DB_PATH),
-            caption="دیتابیس فعلی آهنگ‌ها"
-        )
-        print(f"[ADMIN_HANDLER] Database sent to admin.")
     else:
-        await message.reply("دستور نامعتبر است.")
+        await message.reply("Invalid command.")
