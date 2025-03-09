9aa3bb87c9352dccd1085a0c1f9228102bd00edb
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-10 01:14:08 +0330

    Update private_message_handler.py

--- inlinebot/handlers/private_message_handler.py
+++ inlinebot/handlers/private_message_handler.py
@@ -1,21 +1,8 @@
 from aiogram import types
-from database.user_database import set_user_language, get_user_language
-from utils.language import get_language_keyboard
-from handlers.command_handler import handle_command
+from handlers.search_handler import handle_search
 
 async def handle_private_message(message: types.Message, bot):
-    user_id = message.from_user.id
-
-    # درخواست انتخاب زبان هنگام /start
-    if message.text == "/start":
-        await message.answer("Choose your language:", reply_markup=get_language_keyboard())
-        return
-
-    # تنظیم زبان کاربر
-    if message.text in ["English", "فارسی"]:
-        set_user_language(user_id, message.text)
-        await message.answer(f"Language set to {message.text}.")
-        return
-
-    # پردازش سایر دستورات
-    await handle_command(message, bot)
+    user_input = message.text.strip()
+    
+    if not user_input.startswith("/"):  # اگر پیام یک دستور نبود
+        await handle_search(message, bot)
